4ca9099da1efeb95c0b88c3e7f1ff4ce3dea0311
Duevorn Harris <duevorn@gmail.com>
2016-05-20 16:13:27 -0400

    Morse translation complete

---
+++ js/morse.js
@@ -0,0 +1,24 @@
+var userInput = prompt("Please enter the word you would like translated to morse" +
+"code");
+
+var translatedStatement = "";
+var splitString = userInput.split('');
+
+var alphabet = {
+    'a': '.-',    'b': '-...',  'c': '-.-.', 'd': '-..',
+    'e': '.',     'f': '..-.',  'g': '--.',  'h': '....',
+    'i': '..',    'j': '.---',  'k': '-.-',  'l': '.-..',
+    'm': '--',    'n': '-.',    'o': '---',  'p': '.--.',
+    'q': '--.-',  'r': '.-.',   's': '...',  't': '-',
+    'u': '..-',   'v': '...-',  'w': '.--',  'x': '-..-',
+    'y': '-.--',  'z': '--..',  ' ': '|',
+    '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...', '8': '---..',
+    '9': '----.', '0': '-----'
+};
+
+for(var i = 0; i < splitString.length; i++){
+  translatedStatement += alphabet[splitString[i]] + " ";
+};
+
+document.getElementById('result').innerHTML = translatedStatement;
